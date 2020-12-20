2ead6006cf5a9c6b78d0ad1935ab8a1215acd1d6
BlazGo <61474511+BlazGo@users.noreply.github.com>
2020-12-20 22:43:06 +0100

    Add files via upload

--- Src/main.py
+++ Src/main.py
@@ -4,9 +4,10 @@
 import os
 import time
 import random
+import configparser
 from visaRes import VisVmeter
-import tkinter.filedialog
-import tkinter as tk
+from GUI import simpleUI
+
 
 print("Starting python script...")
 
@@ -33,39 +34,76 @@ class read_write_Func():
         self.interval = 1 # seconds interval of repetition
         self.curr_line = None
         self.old_line = None
-        self.instrument = VisVmeter()
         self.lines_written = 0
 
-        # get path of where this script is located
-        config_path = os.path.join(os.path.basename(os.path.dirname(os.path.realpath(__file__))), "default_config.txt")
-        
-        with open(config_path, "r") as config:
-            config_lines = config.readlines()
-            self.folder_to_read = config_lines[1]
-            self.filename_to_read = config_lines[4]
-            self.folder_to_create = config_lines[7]
-            self.filename_to_create = config_lines[10]
+        self.Tmeter = VisVmeter()
+
+        self.Input_dir_path = ""
+        self.Input_filename = ""
+        self.Output_dir_path = ""
+        self.Output_filename = ""
+        self.Instrument_type = ""       
+
+        try:
+            config = configparser.ConfigParser()
+            config.read("meas_setup.ini")
+
+            file_setup  = config["FILE_SETUP"]            
+            instrument_setup = config["INSTRUMENT_SETUP"]
+
+            self.Input_dir_path = file_setup.get("Input_dir_path")
+            self.Input_filename = file_setup.get("Input_filename")
+            self.Output_dir_path = file_setup.get("Output_dir_path")
+            self.Output_filename = file_setup.get("Output_filename")
+
+            self.Instrument_type = instrument_setup.get("Instrument")
+            self.Meas_num = int(instrument_setup.get("Meas_num"))
+            self.Meas_time = float(instrument_setup.get("Meas_time"))
+            
+            print("Read config file parameters.")
+
+        except:
+            print("Possible error with configuration.")
             pass
 
-        print("\nFolder to read: {}File to read: {}Folder to write: {}New filename: {}\n".format(self.folder_to_read, 
-                                                                                                self.filename_to_read, 
-                                                                                                self.folder_to_create, 
-                                                                                                self.filename_to_create))
+        print("\nDefault:\nFolder to read: {}\nFile to read: {}\nFolder to write: {}\nNew filename: {}\nInstrument: {}\n".format(
+            self.Input_dir_path,     
+            self.Input_filename, 
+            self.Output_dir_path, 
+            self.Output_filename,
+            self.Instrument_type)
+            )
+
+        input_parameters = {
+            "Input dir path"    : self.Input_dir_path,
+            "Input filename"    : self.Input_filename,
+            "Output dir path"   : self.Output_dir_path,
+            "Output filename"   : self.Output_filename,
+            "Instrument"        : self.Instrument_type
+            }
+
+        UI = simpleUI(input_parameters)
+        new_parameters = UI.output_parameter_list
+
+        self.Input_dir_path = new_parameters.get("Input dir path")
+        self.Input_filename = new_parameters.get("Input filename")
+        self.Output_dir_path = new_parameters.get("Output dir path")
+        self.Output_filename =  new_parameters.get("Output filename")
+        self.Instrument_type = new_parameters.get("Instrument")
 
         print("Initialized...\n")
 
     def wait_file(self):
-        status = 0
         # check if wanted file is already created and read it if not wait.
-        while status == 0:
+        while True:
             try:
-                self.old_file = open(os.path.join(self.folder_to_read, self.filename_to_read), "r")
-                self.new_file = open(self.folder_to_create, self.filename_to_create, "a")
+                self.old_file = open(os.path.join(self.Input_dir_path, self.Input_filename), "r")
+                self.new_file = open(self.Output_dir_path, self.Output_filename, "a")
                 print("File created. Moving on...")
                 break
             except:
                 print("No file created yet. Waiting...")
-                time.sleep(self.interval)
+                time.sleep(2)
                 pass
     
     def copy_heading(self):
@@ -88,10 +126,9 @@ class read_write_Func():
             print("No new line...")
         elif self.curr_line is not self.old_line:
             print("New line detected. Writing...")
-            temp = self.instrument.getMeasurement() # Get temp value
+            temp = self.Tmeter.getMeasurement() # Get temp value
             self.copy_line(self.new_file, self.curr_line, temp)
-
-        self.old_line = self.curr_line
+            self.old_line = self.curr_line
     
     def copy_line(self, file_to_write, line, temp = 0):
         file_to_write.write(line.strip("\n") + "\t" + str(round(temp, 2)) + "\n") # Vrite the read line and add temperature at the end
@@ -105,19 +142,22 @@ class read_write_Func():
         self.new_file.close()
         print("Done.")
 
-rw = read_write_Func()
-rw.wait_file()
-rw.copy_heading()
 
-try:
-    while True:
-        start = time.time() # Loop timing
-        rw.check_for_newline()
-        print("Time taken: %.3f [ms]\n" % round((time.time()-start)*1000,3))
-        time.sleep(rw.interval)
+if __name__ == "__main__":
+        
+    rw = read_write_Func()
+    rw.wait_file()
+    rw.copy_heading()
 
-except KeyboardInterrupt:
-    print("Stopping...")
-    pass
+    try:
+        while True:
+            start = time.time() # Loop timing
+            rw.check_for_newline()
+            print("Time taken: %.3f [ms]\n" % round((time.time()-start)*1000,3))
+            time.sleep(rw.interval)
 
-rw.close_files()
+    except KeyboardInterrupt:
+        print("Stopping...")
+        pass
+
+    rw.close_files()
